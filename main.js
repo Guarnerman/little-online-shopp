715eaa50f2fd0e0c4b1ad1ed6d385cfa22e7e147
guarnerman <gs.ulises18@gmail.com>
2023-07-05 18:48:41 -0600

    "se añadió funcionalidad a los menú desplegables entre sí con JS"

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuKart = document.querySelector('.navbar-shopping-kart');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 burguerMenu.addEventListener('click', toogleMobileMenu);
+menuKart.addEventListener('click', toogleKartAside);
 
 function toogleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive')
+  desktopMenu.classList.toggle('inactive');
+  /* mobileMenu.classList.add('inactive'); */
+  aside.classList.add('inactive');
 }
 
 function toogleMobileMenu() {
-  mobileMenu.classList.toggle('inactive')
+  /* desktopMenu.classList.add('inactive'); */
+  mobileMenu.classList.toggle('inactive');
+  aside.classList.add('inactive');
+}
+
+function toogleKartAside() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');
 }
